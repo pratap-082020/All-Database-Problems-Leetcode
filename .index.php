43a202105e38e7213934c36f0ee89ef0bc05f953
pratap-082020 <pratap.082020@users.noreply.github.com>
2021-01-07 15:02:39 +0530

    Final Push
    ALl problems are setup now

---
+++ .index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <link rel="shortcut icon" href="./.favicon.ico">
+   <title>All Database Problems: Leetcode</title>
+   <link rel="stylesheet" href="./.style.css">
+   <script src="./.sorttable.js"></script>
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<!-- Compiled and minified JavaScript -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+
+<style>
+body {
+	font-family: 'Cairo', sans-serif;
+	color: #5F5F5F;
+}
+
+h5 {
+	color: #5F5F5F;
+	font-weight: 900;
+}
+ul li {
+	text-align: left;
+}
+
+a {
+	font-size: 15px;
+	color: #5F5F5F;
+}
+
+</style>
+
+</head>
+
+<body>
+
+
+<div class="navbar-fixed">
+    <nav class="#1a237e indigo darken-4">
+      <div class="container nav-wrapper">
+        <ul class="left hide-on-med-and-down">
+		<i class="large material-icons">code</i>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+<div id="container" class="container">
+<h5>All Database Problems: Leetcode</h5>
+<p>Copyright © 2021 LeetCode</p>
+	
+
+	<table class="sortable">
+	    <thead>
+		<tr>
+			<th>Filename</th>
+			<th>Type</th>
+			<th>Size</th>
+			<th>Date Modified</th>
+		</tr>
+	    </thead>
+	    <tbody><?php
+
+	// Adds pretty filesizes
+	function pretty_filesize($file) {
+		$size=filesize($file);
+		if($size<1024){$size=$size." Bytes";}
+		elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
+		elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
+		else{$size=round($size/1073741824, 1)." GB";}
+		return $size;
+	}
+
+ 	// Checks to see if veiwing hidden files is enabled
+	if($_SERVER['QUERY_STRING']=="hidden")
+	{$hide="";
+	 $ahref="./";
+	 $atext="Hide";}
+	else
+	{$hide=".";
+	 $ahref="./?hidden";
+	 $atext="Show";}
+
+	 // Opens directory
+	 $myDirectory=opendir(".");
+
+	// Gets each entry
+	while($entryName=readdir($myDirectory)) {
+	   $dirArray[]=$entryName;
+	}
+
+	// Closes directory
+	closedir($myDirectory);
+
+	// Counts elements in array
+	$indexCount=count($dirArray);
+
+	// Sorts files
+	sort($dirArray);
+
+	// Loops through the array of files
+	for($index=0; $index < $indexCount; $index++) {
+
+	// Decides if hidden files should be displayed, based on query above.
+	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
+
+	// Resets Variables
+		$favicon="";
+		$class="file";
+
+	// Gets File Names
+		$name=$dirArray[$index];
+		$namehref=$dirArray[$index];
+
+	// Gets Date Modified
+		$modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
+		$timekey=date("YmdHis", filemtime($dirArray[$index]));
+
+
+	// Separates directories, and performs operations on those directories
+		if(is_dir($dirArray[$index]))
+		{
+				$extn="&lt;Directory&gt;";
+				$size="&lt;Directory&gt;";
+				$sizekey="0";
+				$class="dir";
+
+			// Gets favicon.ico, and displays it, only if it exists.
+				if(file_exists("$namehref/favicon.ico"))
+					{
+						$favicon=" style='background-image:url($namehref/favicon.ico);'";
+						$extn="&lt;Website&gt;";
+					}
+
+			// Cleans up . and .. directories
+				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
+				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
+		}
+
+	// File-only operations
+		else{
+			// Gets file extension
+			$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
+
+			// Prettifies file type
+			switch ($extn){
+				case "png": $extn="PNG Image"; break;
+				case "jpg": $extn="JPEG Image"; break;
+				case "jpeg": $extn="JPEG Image"; break;
+				case "svg": $extn="SVG Image"; break;
+				case "gif": $extn="GIF Image"; break;
+				case "ico": $extn="Windows Icon"; break;
+
+				case "txt": $extn="Text File"; break;
+				case "log": $extn="Log File"; break;
+				case "htm": $extn="HTML File"; break;
+				case "html": $extn="HTML File"; break;
+				case "xhtml": $extn="HTML File"; break;
+				case "shtml": $extn="HTML File"; break;
+				case "php": $extn="PHP Script"; break;
+				case "js": $extn="Javascript File"; break;
+				case "css": $extn="Stylesheet"; break;
+
+				case "pdf": $extn="PDF Document"; break;
+				case "xls": $extn="Spreadsheet"; break;
+				case "xlsx": $extn="Spreadsheet"; break;
+				case "doc": $extn="Microsoft Word Document"; break;
+				case "docx": $extn="Microsoft Word Document"; break;
+
+				case "zip": $extn="ZIP Archive"; break;
+				case "htaccess": $extn="Apache Config File"; break;
+				case "exe": $extn="Windows Executable"; break;
+
+				default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} break;
+			}
+
+			// Gets and cleans up file size
+				$size=pretty_filesize($dirArray[$index]);
+				$sizekey=filesize($dirArray[$index]);
+		}
+
+	// Output
+	 echo("
+		<tr class='$class'>
+			<td><a href='./$namehref'$favicon class='name'>$name</a></td>
+			<td><a href='./$namehref'>$extn</a></td>
+			<td sorttable_customkey='$sizekey'><a href='./$namehref'>$size</a></td>
+			<td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
+		</tr>");
+	   }
+	}
+	?>
+	    </tbody>
+	</table>
+</div>
+
+
+<footer class="#1a237e indigo darken-4 page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12" style="font-family: 'Satisfy', cursive;">
+                <h3 class="center white-text">“First, solve the problem. Then, write the code.”</h3>
+                <h5 class="right grey-text text-lighten-4">– John Johnson</h5>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">My Profile</h5>
+                <ul>
+                  <li><img src="https://img.icons8.com/fluent/20/000000/github.png"/><a class="grey-text text-lighten-3" href="https://github.com/pratap-082020"> Github</a></li>
+                  <li><img src="https://img.icons8.com/fluent/20/000000/code.png"/><a class="grey-text text-lighten-3" href="https://github.com/pratap-082020"> Leetcode</a></li>
+                  <li><img src="https://img.icons8.com/color/20/000000/facebook-circled--v1.png"/><a class="grey-text text-lighten-3" href="https://www.facebook.com/profile.php?id=100012859302881"> Facebook</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class=" center container">
+            Copyright © 2021 LeetCode: Please visit <a style="color:white;" href="https://leetcode.com/">Leetcode.com</a> to unlock all problems
+            </div>
+          </div>
+        </footer>
+
+
+</body>
+</html>
